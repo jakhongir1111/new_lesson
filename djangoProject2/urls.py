96b75d22aa3c4913e4a215cf3644286bae7606e3
jakhongir1111 <jakhongirbek4015>
2023-07-07 13:49:10 +0500

    second push

--- djangoProject2/urls.py
+++ djangoProject2/urls.py
@@ -6,7 +6,8 @@ from apps.views import index, detail
 from djangoProject2 import settings
 
 urlpatterns = [
-    path('', index),
-    path('post/<str:slug>', detail, name='detail'),
-    path('admin/', admin.site.urls),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+                  path('', index),
+                  path('post/<str:slug>', detail, name='detail'),
+                  path('admin/', admin.site.urls),
+              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL,
+                                                                                           document_root=settings.MEDIA_ROOT)
